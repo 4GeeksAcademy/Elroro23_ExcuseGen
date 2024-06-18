00951b4ddc2aa81cc615bd1e24ac070b9dd42cf3
Elroro23 <rodolfodalessandro23@gmail.com>
2024-06-18 17:20:04 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+//importando archivos.
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  //La función window.onload se ejecuta cuando toda la página ha terminado de cargar.
   console.log("Hello Rigo from the console!");
+  //Variables que contienen Arrays de strings
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+  function randomGen(list) {
+    //Esta función toma una lista (array) y devuelve un elemento aleatorio de esa lista.
+    return list[Math.floor(Math.random() * list.length)]; //Math.random() genera un número aleatorio entre 0 y 1.
+
+    //Math.floor() redondea ese número hacia abajo al número entero más cercano.
+    //Multiplicamos Math.random() por la longitud de la lista para obtener un índice válido de la lista.
+  }
+  let whoRandom = randomGen(who); //Aquí estamos usando la función randomGen para obtener un elemento aleatorio de cada lista y guardarlo en variables.
+  let actionRandom = randomGen(action);
+  let whatRandom = randomGen(what);
+  let whenRandom = randomGen(when);
+
+  let excuse = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`; //Usamos template literals(`$`) para construir la frase con los elementos aleatorios seleccionados.
+  document.getElementById("excuse").innerText = excuse; // Muestra la frase del HTML en la página web.
 };
